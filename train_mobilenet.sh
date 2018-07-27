6f9f0a9676beef3e8ac010a4b2436ddffc73463f
tentenymy <meiyiyang1201@gmail.com>
2018-07-27 10:28:42 -0700

    mobilenet1_emore_0

--- train_mobilenet.sh
+++ train_mobilenet.sh
@@ -1,8 +1,3 @@
-# environment
-export MXNET_CPU_WORKER_NTHREADS=24
-export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
-export MXNET_ENGINE_TYPE=ThreadedEnginePerDevice
-
 # gpu & batch
 CUDA='1,3'
 BATCH_SIZE='152'
@@ -22,4 +17,4 @@ MOM='0.9'
 TARGET=lfw,classify_megaface
 LOGFILE='/home/meiyi/Face/insightface/output/log/log_'$TAG'_arg.txt'
 
-export CUDA_VISIBLE_DEVICES=$CUDA; python -u ~/Face/insightface/src/train_softmax_my.py --network $NETWORK --loss-type 5 --margin-m 0.5 --per-batch-size $BATCH_SIZE --verbose 1000 --tag $TAG --target $TARGET --data-dir $DATA_DIR  --prefix $MODEL_OUTPUT_DIR --emb-size $EMB_SIZE --fc7-wd-mult $FC7_WD_MULT --mom $MOM > "$LOGFILE"
+export CUDA_VISIBLE_DEVICES=$CUDA; python -u ~/Face/insightface/src/train_softmax_my.py --network $NETWORK --loss-type 5 --margin-m 0.5 --per-batch-size $BATCH_SIZE --verbose 1000 --tag $TAG --target $TARGET --data-dir $DATA_DIR  --prefix $MODEL_OUTPUT_DIR --emb-size $EMB_SIZE --fc7-wd-mult $FC7_WD_MULT --mom $MOM
