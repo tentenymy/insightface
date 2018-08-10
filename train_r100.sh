6b8bfb2802a0aa43615706f7b97ecf239f7ebe60
tentenymy <meiyiyang1201@gmail.com>
2018-08-10 15:48:57 -0700

    r100_triplet

--- train_r100.sh
+++ train_r100.sh
@@ -1,16 +1,16 @@
-CUDA='0,1,2,3,4,5'
+CUDA='2,3,4,5,6,7'
 BATCH_SIZE='64'
 
 DATA_DIR=~/Face/insightface/datasets/faces_emore
-MODEL_OUTPUT_DIR=/data/meiyi/models/r100/model_r100_emore_v3_1/model_r100_emore_v3
+MODEL_OUTPUT_DIR=/data/meiyi/models/r100/model_r100_emore_v3_0/model_r100_emore_v3
 TAG=r100_emore_v3
 
-LAST_EPOCH=0
-PRETRAIN=/data/meiyi/models/r100/model_r100_emore_v2_2/model_r100_emore_v2,20
+LAST_EPOCH=92
+PRETRAIN=/data/meiyi/models/r100/model_r100_emore_v3_0/model_r100_emore_v3,92
 
 NETWORK=r100
 
-TARGET=lfw,classify_megaface,classify_suning_test
+TARGET=classify_megaface,lfw
 #LOGFILE='/home/Face/insightface/output/log/log_'$TAG'_arg.txt'
 
 export CUDA_VISIBLE_DEVICES=$CUDA; python -u ~/Face/insightface/src/train_softmax_my.py --network $NETWORK --loss-type 5 --margin-m 0.5 --per-batch-size $BATCH_SIZE --verbose 1000 --tag $TAG --target $TARGET --data-dir $DATA_DIR  --prefix $MODEL_OUTPUT_DIR --last_epoch $LAST_EPOCH --pretrained $PRETRAIN
--- train_r100_triplet.sh
+++ train_r100_triplet.sh
@@ -1,19 +1,20 @@
-CUDA='0,1,2,3,4,5'
+CUDA='2,3,4,5,6,7'
 BATCH_SIZE=75
 
 DATA_DIR=/home/meiyiyang/Face/insightface/datasets/faces_emore
 MODEL_OUTPUT_DIR=/data/meiyi/models/r100/model_r100_emore_v3_1/model_r100_emore_v3
 TAG=r100_emore_v3
 
-LAST_EPOCH=12
-PRETRAIN=/data/meiyi/models/r100/model_r100_emore_v3_1/model_r100_emore_v3,12
+LAST_EPOCH=0
+PRETRAIN=/data/meiyi/models/r100/model_r100_emore_v3_0/model_r100_emore_v3,107
 
 NETWORK=r100
 MOM='0.0'
 LOSS_TYPE=12
-TRIPLET_BAG_SIZE=2700
+TRIPLET_BAG_SIZE=3600
+TRIPLET_ALPHA=0.30
 
 TARGET=classify_megaface,lfw,classify_suning_test
 LOGFILE='/home/meiyiyang/Face/insightface/output/log/log_'$TAG'_arg.txt'
 
-export CUDA_VISIBLE_DEVICES=$CUDA; python -u ~/Face/insightface/src/train.py --network $NETWORK --loss-type $LOSS_TYPE --margin-m 0.5 --per-batch-size $BATCH_SIZE --verbose 1000 --target $TARGET --data-dir $DATA_DIR  --prefix $MODEL_OUTPUT_DIR --mom $MOM --pretrained $PRETRAIN --lr 0.005 --tag $TAG --last_epoch $LAST_EPOCH --triplet-bag-size $TRIPLET_BAG_SIZE
+export CUDA_VISIBLE_DEVICES=$CUDA; python -u ~/Face/insightface/src/train.py --network $NETWORK --loss-type $LOSS_TYPE --margin-m 0.5 --per-batch-size $BATCH_SIZE --verbose 1000 --target $TARGET --data-dir $DATA_DIR  --prefix $MODEL_OUTPUT_DIR --mom $MOM --pretrained $PRETRAIN --lr 0.005 --tag $TAG --last_epoch $LAST_EPOCH --triplet-bag-size $TRIPLET_BAG_SIZE --triplet-alpha $TRIPLET_ALPHA
